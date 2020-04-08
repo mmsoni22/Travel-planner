6d39618672565395b19504da3bec0eac6868e28d
mmsoni22 <50927689+mmsoni22@users.noreply.github.com>
2020-04-08 13:34:10 -0400

    sass loader

--- src/client/index.js
+++ src/client/index.js
@@ -2,3 +2,8 @@ import { handlesubmit } from "./js/formHandler"
 import { checkForName } from "./js/nameChecker"
 
 alert("This is an alert!");
+
+import './styles/reset.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/form.scss';
--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({urlencoded : false}));
 
 app.use(express.static("dist"));
 
-const port = 8080;
+const port = 3000;
 
 app.listen(port , ()=> {
     console.log(`Travel app is running on port ${port}`);
