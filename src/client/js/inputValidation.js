28c6f3fe16bb028fdbd83db869d6f97c9036c64d
mmsoni22 <50927689+mmsoni22@users.noreply.github.com>
2020-04-10 15:46:56 -0400

    Date validation function

--- src/client/js/inputValidation.js
+++ src/client/js/inputValidation.js
@@ -1,23 +1,55 @@
-const validDestination = (destination) => {
-    let valid = false; 
 
-    return valid;
-  
-const validDate = (date) => {
-    let valid = false;
-    let currentDate = new date();
-    const dateRegexExp = /^(0[0-9]|1[012])[\/\-](0[0-9]|[12][0-9]|3[01])[\/\-]\d{4}$/; //date format mm/dd/yyyy
+const validDestination = ( destination ) => {
 
-    if(date.dateRegexExp.test(date) ) {
+	let valid = false;
 
-        //validation code 
-        
-        valid = true;
-    }else {
-        alert("Please enter valid date format");
-    }
+	if( destination != '' ){
+
+		valid = true;
+
+	}else{
+
+		alert( 'Please add a valid destination.' );
+
+	}
+
+	return valid;
+
+}
+const validDate = ( date ) => {
+
+	let valid = false;
+	const dateRegExp = /^(0[1-9]|1[012])[\/\-](0[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/; 
+	
+	if( dateRegExp.test( date ) ){ // Check for proper date format.
+
+		let currentDate = new Date();
+
+		// Create date object with user input.
+		const dateArray = date.split( '/' );
+		let requestDate = new Date( parseInt( dateArray[2] ), parseInt( dateArray[0] ) - 1, parseInt( dateArray[1] ) ); // Year, Month Index, Day. Month index = month - 1.
+
+		if( ( requestDate - currentDate ) > 0 ){
+
+			valid = true;
+
+		}else{
+
+			alert( 'Please enter a date that is in the future.' );
+
+		}
+
+	}else{
+
+		alert( 'Please enter a valid date format.' );
+
+	}
+
+	return valid;
 
 }
 
-    return valid
+export{
+	validDestination,
+	validDate
 }
