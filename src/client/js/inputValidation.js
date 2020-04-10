3df63d2c2757558c5e5d6292820976e9ec841afa
mmsoni22 <50927689+mmsoni22@users.noreply.github.com>
2020-04-10 14:12:30 -0400

    ADD date regex

--- src/client/js/inputValidation.js
+++ src/client/js/inputValidation.js
@@ -2,11 +2,22 @@ const validDestination = (destination) => {
     let valid = false; 
 
     return valid;
-
-}
-
+  
 const validDate = (date) => {
     let valid = false;
+    let currentDate = new date();
+    const dateRegexExp = /^(0[0-9]|1[012])[\/\-](0[0-9]|[12][0-9]|3[01])[\/\-]\d{4}$/; //date format mm/dd/yyyy
+
+    if(date.dateRegexExp.test(date) ) {
+
+        //validation code 
+        
+        valid = true;
+    }else {
+        alert("Please enter valid date format");
+    }
+
+}
 
     return valid
 }
